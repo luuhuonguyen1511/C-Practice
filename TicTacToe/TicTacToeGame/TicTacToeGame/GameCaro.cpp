7d16e75ae268b44dd23988782b0eec3a2ca94377
Uyen <luuhuonguyen1511@gmail.com>
2018-12-28 11:09:07 +0700

    *Fix exit error/ Change size of array to const/ Rewrite command mark again

--- TicTacToe/TicTacToeGame/TicTacToeGame/GameCaro.cpp
+++ TicTacToe/TicTacToeGame/TicTacToeGame/GameCaro.cpp
@@ -1,15 +1,18 @@
-#include <C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include\iostream>
+#include <iostream>
+#include <cstdlib>
+#define arraySize 3
+#define nameSize 20
 
 using namespace std;
 
-void drawBoard(char board[3][3]);
-void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20], string result);
-bool check(char board[3][3], int a, int b);
-int gameStatus(char board[3][3]);
+void drawBoard(char board[arraySize][arraySize]);
+void startGame(char board[arraySize][arraySize], char nameOfPlayer1[nameSize], char nameOfPlayer2[nameSize], string result);
+bool checkEmpty(char board[arraySize][arraySize], int a, int b);
+int getGameStatus(char board[arraySize][arraySize]);
 
-void drawBoard(char board[3][3]) {
+void drawBoard(char board[arraySize][arraySize]) {
 	cout << " _____" << " " << "_____" << " " << "_____" << endl;
-	for (int i = 0; i<3; i++)
+	for (int i = 0; i<arraySize; i++)
 		{
 			cout <<"|" << "     " << "|" << "     " << "|" << "     " << "|" << endl;
 			cout << "|" <<"  "<<board[i][0]<<"  "<<"|"<<"  "<<board[i][1]<<"  "<<"|"<<"  "<<board[i][2]<<"  "<< "|"<<endl;
@@ -17,15 +20,15 @@ void drawBoard(char board[3][3]) {
 		}
 }
 
-bool check(char board[3][3], int a, int b) {
+bool checkEmpty(char board[arraySize][arraySize], int a, int b) {
 	if (board[a][b] == ' ') return true;
 	else return false;
 }
 
-int gameStatus(char board[3][3]) {
+int getGameStatus(char board[arraySize][arraySize]) {
 	int win = 0;
 	if (win == 0) {
-		for (int i = 0; i < 3; i++) {
+		for (int i = 0; i < arraySize; i++) {
 			switch (board[i][0])
 			{
 			case 'O':
@@ -42,7 +45,7 @@ int gameStatus(char board[3][3]) {
 		}
 	
 		if (win == 0) {
-			for (int j = 0; j < 3; j++) {
+			for (int j = 0; j < arraySize; j++) {
 				switch (board[0][j])
 				{
 				case 'O':
@@ -92,7 +95,7 @@ int gameStatus(char board[3][3]) {
 	return win;
 }
 
-void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20], string result) {
+void startGame(char board[arraySize][arraySize], char nameOfPlayer1[nameSize], char nameOfPlayer2[nameSize], string result) {
 	int player = 1;
 	int position, row, column;
 	int count = 0;
@@ -102,19 +105,27 @@ void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20],
 		switch (player)
 		{
 		case 1:
-			do {
-				cout << "Your turn " << nameOfPlayer1 << " - O: ";
-				cin >> position;
+			cout << "Your turn " << nameOfPlayer1 << " - O: ";
+			cin >> position;
 
-				row = position / 10 - 1;
-				column = position % 10 - 1;
+			row = position / 10 - 1;
+			column = position % 10 - 1;
 
-				if (check(board, row, column)) {
-					board[row][column] = 'O';
-					break;
-				}
-			} while (check(board, row, column)==false);
+			if (checkEmpty(board, row, column)) {
+				board[row][column] = 'O';
+			}
+			else
+			{
+				do {
+					cout << "Please mark again - O: ";
+					cin >> position;
 
+					row = position / 10 - 1;
+					column = position % 10 - 1;
+				} while (checkEmpty(board, row, column)==false);
+				board[row][column] = 'O';
+			}
+				
 			system("cls");
 			drawBoard(board);
 
@@ -122,18 +133,26 @@ void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20],
 			player++;
 			break;
 		case 2:
-			do {
-				cout << "Your turn " << nameOfPlayer2 << " - X: ";
-				cin >> position;
+			cout << "Your turn " << nameOfPlayer2 << " - X: ";
+			cin >> position;
 
-				row = position / 10 - 1;
-				column = position % 10 - 1;
+			row = position / 10 - 1;
+			column = position % 10 - 1;
 
-				if (check(board, row, column)) {
-					board[row][column] = 'X';
-					break;
-				}
-			} while (check(board, row, column) == false);
+			if (checkEmpty(board, row, column)) {
+				board[row][column] = 'X';
+			}
+			else
+			{
+				do {
+					cout << "Please mark again - X: ";
+					cin >> position;
+
+					row = position / 10 - 1;
+					column = position % 10 - 1;
+				} while (checkEmpty(board, row, column) == false);
+				board[row][column] = 'X';
+			}
 
 			system("cls");
 			drawBoard(board);
@@ -149,7 +168,7 @@ void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20],
 			break;
 		}
 
-		switch (gameStatus(board))
+		switch (getGameStatus(board))
 			{
 			case 1: cout << "PLAYER 1 WIN!!!" << endl;
 				result = 'win';
@@ -167,9 +186,9 @@ void startGame(char board[3][3], char nameOfPlayer1[20], char nameOfPlayer2[20],
 int main() {
 
 	char choice, confirm;
-	static char nameOfPlayer1[20]; 
-	static char nameOfPlayer2[20];
-	static char board[3][3] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+	static char nameOfPlayer1[nameSize]; 
+	static char nameOfPlayer2[nameSize];
+	static char board[arraySize][arraySize] = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
 	static string result = "playing";
 
 	
@@ -178,24 +197,25 @@ int main() {
 	cout << "If you find any problem, please contact john.nguyen@gameloft.com" << endl;
 
 	cout << "SELECT YOUR MODE (1 - PLAY GAME, OTHERS - EXIT GAME): "; cin >> choice;
-
-	cin.ignore();
-	cout << "Enter Player 1 Name: "; cin.getline(nameOfPlayer1,20) ;
-	cout << "Enter Player 2 Name: "; cin.getline(nameOfPlayer2,20);	
+	
+	
 	do {
 		system("cls");
-		for (int i = 0; i < 3; i++)
-			for (int j = 0; j < 3; j++)
+		for (int i = 0; i < arraySize; i++)
+			for (int j = 0; j < arraySize; j++)
 				board[i][j] = ' ';
-
+			
 		switch (choice)
 		{
 		case '1':
+			cin.ignore();
+			cout << "Enter Player 1 Name: "; cin.getline(nameOfPlayer1,nameSize) ;
+			cout << "Enter Player 2 Name: "; cin.getline(nameOfPlayer2,nameSize);	
 			drawBoard(board);
 			startGame(board, nameOfPlayer1, nameOfPlayer2, result);
 			break;
 		default:
-			break;
+			exit(0);
 		}
 		
 		cout << "Do you wanna play again? (y/n): "; cin >> confirm;
